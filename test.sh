f7032098abb103930049cc5868c50acdbce7b3c5
Marc Titinger <mtitinger@baylibre.com>
2016-01-29 16:37:09 +0100

    rsync the folder passed as arg2 of stop hook to powerci
    Signed-off-by: Marc Titinger <mtitinger@baylibre.com>

--- test.sh
+++ test.sh
@@ -1,1 +1,15 @@
-./iio-capture -n lab-baylibre-acme.local iio:device1
+
+
+
+#./iio-capture -n lab-baylibre-acme.local iio:device1
+
+set -x 
+
+./iio-probe-start 1 aaabbbcccdddeee-0 &
+sleep 10
+
+./iio-probe-stop 1 aaabbbcccdddeee
+
+
+
+
